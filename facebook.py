6a5605909d337e00b1d959bb41b49818ebb5d106
Alexander Vu <avu@clarku.edu>
2024-10-19 10:26:21 -0400

    Facebook update
    Facebook returns a pd.Dataframe

--- facebook.py
+++ facebook.py
@@ -1,15 +1,16 @@
 import requests
+import pandas as pd
 
 def get_groups(keyword):
     # URL to search for public groups containing the keyword
     url = f'https://www.googleapis.com/customsearch/v1?key=AIzaSyDmzZ5B86WPjcUpBghQ-F2Gg95T0dHmAiE&cx=70c8a26a4782b4147&q={keyword}+facebook+group'
 
-    response = requests.get(url).json()
+    response = requests.get(url).json()["items"]
 
-    top_groups = {}
-    
-    for i in range(3):
-        top_groups[response["items"][i]["title"]] = response["items"][i]["link"]
+    top_groups = pd.DataFrame({
+        "name" : [response[i]["title"] for i in range(5)],
+        "description" : [response[i]["pagemap"]["metatags"][0]["og:description"] for i in range(5)]
+    })
     
     return top_groups
 
