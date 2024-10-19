8681a684a9838c4fc661370a4d2a40047da2eea4
Alexander Vu <avu@clarku.edu>
2024-10-19 15:50:03 -0400

    gemini event finding
    gemini find_events returns any events within a community

--- gemini.py
+++ gemini.py
@@ -1,7 +1,6 @@
 import google.generativeai as gem
 
 def summary_from_post_text(community,text):
-    import google.generativeai as gem
     gem.configure(api_key="AIzaSyA8XQWC7KyDbZozq7iLzJh-n9Ptusn3NP0")
     model = gem.GenerativeModel("gemini-1.5-flash",system_instruction=f"You are going to be sent posts from a community called {community}"+
                                 " summarize the vibe and conversation in less than 3 sentences. Do not use the word vibrant.")
@@ -9,7 +8,6 @@ def summary_from_post_text(community,text):
     return model_response.text
 
 def summary_from_desc(group, text):
-    import google.generativeai as gem
     gem.configure(api_key="AIzaSyA8XQWC7KyDbZozq7iLzJh-n9Ptusn3NP0")
     model = gem.GenerativeModel("gemini-1.5-flash",system_instruction=f"You are going to be sent a description of a facebook group called {group}"+
                                 " summarize and rewrite the description in less than 3 sentences")
@@ -17,9 +15,17 @@ def summary_from_desc(group, text):
     return model_response.text
 
 def summary_tumblr(group, text):
-    import google.generativeai as gem
     gem.configure(api_key="AIzaSyA8XQWC7KyDbZozq7iLzJh-n9Ptusn3NP0")
     model = gem.GenerativeModel("gemini-1.5-flash",system_instruction=f"You are going to be sent a description of a tumblr group called {group}"+
                                 " summarize and rewrite the description in less than 3 sentences")
+    model_response = model.generate_content(text)
+    return model_response.text
+
+def find_events(group, text):
+    gem.configure(api_key="AIzaSyA8XQWC7KyDbZozq7iLzJh-n9Ptusn3NP0")
+    model = gem.GenerativeModel(f"gemini-1.5-flash",system_instruction=f"You will be sent text from posts in a community called {group}."+
+                                "If you find an event, respond in the format: Event Found!\nEvent Title/Description at Event Location on Event Date"+
+                                "If you do not respond with: No Events Found"+
+                                "If you find multiple events focus on the most significant one")
     model_response = model.generate_content(text)
     return model_response.text
